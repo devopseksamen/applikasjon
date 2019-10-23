1fff31819aaf1abb7095f94238d98b6f4f0daa21
Your Name <you@example.com>
2019-10-24 00:27:24 +0200

    updated permissions for build

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
-before install:
-- ./gradlew build
+#!/usr/bin/env bash
+
+before_install:
+chmod +x gradlew
+
+after_install:
+./gradlew build
 - docker build -t pgr301
 
 after_success: 
-- docker build -t pgr301
+- ./docker build -t pgr301
